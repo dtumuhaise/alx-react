2e34fa4c54d70fb4742423c486a2f7ae05896e25
Davis Tumuhaise <tumuhaise@gmail.com>
2023-08-08 14:55:14 +0300

    task 2

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.jpg';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  const isIndex = true;
+  return (
+    <div className="App">
+      <div header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+        </div>
+        <hr />
+        <div body className="App-body">
+          <p>Login to access the full dashboard</p>
+          </div>
+          <hr />
+          <div footer className="App-footer">
+           <p>{getFullYear()} - {getFooterCopy(isIndex)} </p>
+            </div>
+    </div>
+  );
+}
+
+export default App;
