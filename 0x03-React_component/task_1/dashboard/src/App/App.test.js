dcb525932b84fd36e1fc9b5d0151aa72946634e4
Davis Tumuhaise <tumuhaise@gmail.com>
2023-08-23 21:29:51 +0300

    test

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -49,3 +49,19 @@ describe("Testing <App isLoggedIn={true} />", () => {
     expect(wrapper.find('CourseList').exists());
   });
 });
+
+  describe("Testing <App logOut={function} />", () => {
+
+    it("verify that when the keys control and h are pressed the logOut function, passed as a prop, is called and the alert function is called with the string Logging you out", () => {
+      const wrapper = mount(<App logOut={()=>{console.log("ctrl and h are pressed")}}/>);
+      window.alert = jest.fn();
+      const inst = wrapper.instance();
+      const logout = jest.spyOn(inst, 'logOut');
+      const alert = jest.spyOn(window, 'alert');
+      const event = new KeyboardEvent('keydown', {bubbles:true, ctrlKey: true, key: 'h'});
+      document.dispatchEvent(event);
+      expect(alert).toBeCalledWith("Logging you out");
+      expect(logout).toBeCalled();
+      alert.mockRestore();
+    });
+});
